ad64757e46ca1d837d667faff8812fdb14722385
PrzemekGdula <przemo.g12@wp.pl>
2019-04-23 11:41:17 +0200

    orders with shipment at any place are ok now

--- orders.test.js
+++ orders.test.js
@@ -1,6 +1,7 @@
 const sumOrderPrice = (order) => order.reduce(
     (priceSum, product, i, arr) => {
-        if (product.name === 'shipment' && priceSum > 100) {
+
+        if (product.name === 'shipment' && (sumOrderPrice(arr.slice(i + 1, arr.length)) + priceSum) > 100) {
             return priceSum
         }
         return priceSum + product.price * (product.quantity || 1)
