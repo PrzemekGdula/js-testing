a539bc6a10c7dc58da74af82cd0968633d483811
PrzemekGdula <przemo.g12@wp.pl>
2019-04-23 10:43:44 +0200

    rounding to 3rd decimal place and negative number tests

--- add.test.js
+++ add.test.js
@@ -1,13 +1,16 @@
-const add = (a, b) => (Number(a) && Number(b)) ? Math.round((Number(a) + Number(b))*1000)/1000 : 0;
+const add = (a, b) => (Number(a) && Number(b)) ? Math.round((Number(a) + Number(b)) * 1000) / 1000 : 0;
+
 if (add(2, 5) !== 7
-|| add(2,10) !== 12
-|| add(0,0) !== 0) {
+    || add(2, 10) !== 12
+    || add(0, 0) !== 0
+    || add(-10, -20) !== -30) {
     throw new Error('Błąd dodawania.')
 }
 console.log('Podstawowe dodawanie liczb całkowitych działa')
 
 if (add('2', '5') !== 7
-|| add('45', '125') !== 170) {
+    || add('45', '125') !== 170
+    || add('20', '-10') !== 10) {
     throw new Error('Błąd dodawania liczb w stringach')
 }
 console.log('Dodawanie liczb w stringach działa')
