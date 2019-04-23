ac3b74a24508abe968fca165ee5d80a10c570510
PrzemekGdula <przemo.g12@wp.pl>
2019-04-23 10:34:33 +0200

    0 for literals

--- add.test.js
+++ add.test.js
@@ -1,22 +1,31 @@
+const add = (a, b) => {
+    const aToNumber = Number(a)
+    const bToNumber = Number(b)
+    if (aToNumber && bToNumber) {
+        return aToNumber + bToNumber
+    }
+    return 0
+}
 
-const add = (a, b) => a + b;
-
-if (add(2, 5) !== 7 || add(2, 10) !== 12 || add(0, 0) !== 0) {
+if (add(2, 5) !== 7
+|| add(2,10) !== 12
+|| add(0,0) !== 0) {
     throw new Error('Błąd dodawania.')
 }
 console.log('Podstawowe dodawanie liczb całkowitych działa')
 
-if (add('2', '5') !== 7 || add('45', '125') !== 170) {
+if (add('2', '5') !== 7
+|| add('45', '125') !== 170) {
     throw new Error('Błąd dodawania liczb w stringach')
 }
 console.log('Dodawanie liczb w stringach działa')
 
-if (add('ala', 'ma kota') !== 0) {
+if (add('ala', 'ma kota') !== 0 || add('ala', 5) !== 0) {
     throw new Error('Funkcja powinna zwracać 0 dla stringów z literami')
 }
 console.log('Funkcja zwraca 0 dla literek')
 
-// if (add(0.2, 0.4) !== 0.6) {
+// if (add(0.2, 0.4) !== 0.6 || add(0.1, 0.01) !== 0.11) {
     // throw new Error('Błąd dodawania liczb zmiennoprzecinkowych')
 // }
 // console.log('Dodawanie liczb zmiennoprzecinkowych działa')
